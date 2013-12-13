ba7e45a279fba1baf4b92a45d5b4896bdf7824dd
justinbangerter <jnbangerter@gmail.com>
2013-12-12 20:29:09 -0500

    Uploaded the script

---
+++ css.js
@@ -0,0 +1,15 @@
+CSS = (function() {
+  var head = document.getElementsByTagName('head')[0];
+  return function (css){
+    var style = document.createElement('style');
+    style.type = 'text/css';
+
+    if (style.styleSheet){
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    head.appendChild(style);
+  }
+})();
